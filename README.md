6746364722cc9325eabf9faf41ba20657aab84f5
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2021-08-09 11:58:10 -0400

    add tepig host

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ Seperated in to `pkgs/` and `gui/` for easy installation on server devices. impo
 
 ### [reshiram](./hosts/reshiram)
 Desktop. Ryzen 5600x CPU, 16gb 3600mhz CL16 memory, Radeon RX 550 4gb GPU.
+
+### [tepig](./hosts/tepig)
+Raspberry Pi 4b. 8gb memory.
+
---
+++ hosts/tepig/configuration.nix
@@ -0,0 +1,83 @@
+{ config, pkgs, ... }:
+
+{
+	imports =
+		[ # Include the results of the hardware scan.
+			./hardware-configuration.nix
+			"${fetchTarball "https://github.com/NixOS/nixos-hardware/archive/7da029f26849f8696ac49652312c9171bf9eb170.tar.gz" }/raspberry-pi/4"
+
+			/home/jeffrey/nixos-configs/nixos/users/jeffrey.nix
+
+			/home/jeffrey/nixos-configs/nixos/base.nix
+			/home/jeffrey/nixos-configs/nixos/pkgs
+
+			/home/jeffrey/nixos-configs/home-manager/base.nix
+		];
+	
+	# import home-manager packages
+	home-manager.users.jeffrey = { ... }: {
+		imports = [
+			/home/jeffrey/nixos-configs/home-manager/pkgs
+		];
+	};
+
+	# Use Raspbery Pi bootloader
+	boot.loader.raspberryPi = {
+		enable = true;
+		version = 4;
+	};
+
+	# Set your time zone.
+	time.timeZone = "America/Indiana/Indianapolis";
+
+	# Define your hostname.
+	networking.hostName = "tepig";
+	networking.networkmanager.enable = true;
+	networking.firewall.trustedInterfaces = [ "eth0" "wlan0" ];
+
+	# Enable Bluetooth
+	hardware.bluetooth.enable = true;
+
+	environment.systemPackages = with pkgs; [
+		wirelesstools
+	];
+
+	# The global useDHCP flag is deprecated, therefore explicitly set to false here.
+	# Per-interface useDHCP will be mandatory in the future, so this generated config
+	# replicates the default behaviour.
+	networking.useDHCP = false;
+	networking.interfaces.eth0.useDHCP = true;
+	networking.interfaces.wlan0.useDHCP = true;
+
+	# Select internationalisation properties.
+	i18n.defaultLocale = "en_US.UTF-8";
+	console = {
+		font = "Lat2-Terminus16";
+		keyMap = "us";
+	};
+
+	# tepig.local
+	services.avahi.publish.enable = true;
+	services.avahi.publish.addresses = true;
+	services.avahi.publish.workstation = true;
+
+	# Some programs need SUID wrappers, can be configured further or are
+	# started in user sessions.
+	programs.mtr.enable = true;
+	programs.gnupg.agent = {
+		enable = true;
+		enableSSHSupport = true;
+	};
+
+	# Enable the OpenSSH daemon.
+	services.openssh.enable = true;
+
+	# This value determines the NixOS release from which the default
+	# settings for stateful data, like file locations and database versions
+	# on your system were taken. It‘s perfectly fine and recommended to leave
+	# this value at the release version of the first install of this system.
+	# Before changing this value read the documentation for this option
+	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+	system.stateVersion = "21.11"; # Did you read the comment?
+}
+
