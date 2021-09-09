dea547309e5941eb3fc7a19fab599fee4c314083
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2021-09-09 14:28:18 -0400

    add hard drive to reshiram

--- hosts/reshiram/hardware-configuration.nix
+++ hosts/reshiram/hardware-configuration.nix
@@ -8,7 +8,7 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" ];
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "ahci" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
@@ -17,6 +17,12 @@
     { device = "/dev/disk/by-uuid/333b1c2c-db0d-4d01-bb48-06d70d3b69a9";
       fsType = "btrfs";
       options = [ "subvol=@" ];
+    };
+
+  fileSystems."/games" =
+    { device = "/dev/disk/by-uuid/333b1c2c-db0d-4d01-bb48-06d70d3b69a9";
+      fsType = "btrfs";
+      options = [ "subvol=@games" ];
     };
 
   fileSystems."/home" =
@@ -30,8 +36,8 @@
       fsType = "vfat";
     };
 
-  fileSystems."/games" =
-    { device = "/dev/disk/by-uuid/333b1c2c-db0d-4d01-bb48-06d70d3b69a9";
+  fileSystems."/storage/games" =
+    { device = "/dev/disk/by-uuid/1c2d6f58-f00f-4a6e-a52b-d765b222f1f5";
       fsType = "btrfs";
       options = [ "subvol=@games" ];
     };
@@ -40,6 +46,4 @@
     [ { device = "/dev/disk/by-uuid/2adf0dd9-0942-4b49-9a18-b4d5f273926b"; }
     ];
 
-  # high-resolution display
-  hardware.video.hidpi.enable = lib.mkDefault true;
 }
