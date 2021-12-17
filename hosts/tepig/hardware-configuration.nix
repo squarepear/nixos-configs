b197a21d8cc03c07148f314db57442aaa1e1d97a
Jeffrey Harmon <contact@jeffreyharmon.dev>
2021-12-17 00:49:42 -0500

    add usb storage to tepig

--- hosts/tepig/hardware-configuration.nix
+++ hosts/tepig/hardware-configuration.nix
@@ -1,11 +1,13 @@
 { ... }:
 
 {
-  fileSystems = {
-    "/" = {
-      device = "/dev/disk/by-label/NIXOS_SD";
+  fileSystems."/" =
+    { device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
       fsType = "ext4";
-      options = [ "noatime" ];
     };
-  };
+
+  fileSystems."/cluster-nfs" =
+    { device = "/dev/disk/by-uuid/d4d0f63e-cd6c-4947-8b81-bae9322877d9";
+      fsType = "ext4";
+    };
 }
