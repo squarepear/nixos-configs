92aa9ed5692eed4bee4af13c9d485d1b5e7f2fc7
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2022-01-02 12:52:11 -0500

    enable flakes

--- nixos/base.nix
+++ nixos/base.nix
@@ -6,4 +6,9 @@
 
   # Allow unfree packages (vscode, steam, ...)
   nixpkgs.config.allowUnfree = true;
+
+  # Enable flakes
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
 }
