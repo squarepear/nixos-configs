fbd26e0ec8bcdbc6436518cdb37d92eb1b4a3acb
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2022-01-15 11:43:20 -0500

    fix formatting

--- hosts/tepig/nixos/hardware-configuration.nix
+++ hosts/tepig/nixos/hardware-configuration.nix
@@ -2,12 +2,14 @@
 
 {
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
+    {
+      device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
       fsType = "ext4";
     };
 
   fileSystems."/cluster-nfs" =
-    { device = "/dev/disk/by-uuid/d4d0f63e-cd6c-4947-8b81-bae9322877d9";
+    {
+      device = "/dev/disk/by-uuid/d4d0f63e-cd6c-4947-8b81-bae9322877d9";
       fsType = "ext4";
     };
 }
