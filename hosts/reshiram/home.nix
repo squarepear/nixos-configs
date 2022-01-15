a7ae704f45eee56e19cdad29f6ef5fce71fa580f
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2022-01-15 12:39:32 -0500

    switch to flake based config

---
+++ hosts/reshiram/home.nix
@@ -0,0 +1,20 @@
+{ ... }:
+
+{
+  imports = [
+    ../../config/home
+
+    ../../config/home/sway
+    ../../config/home/bat.nix
+    ../../config/home/direnv.nix
+    ../../config/home/firefox.nix
+    ../../config/home/fonts.nix
+    ../../config/home/git.nix
+    ../../config/home/kitty.nix
+    ../../config/home/neovim.nix
+    ../../config/home/obs.nix
+    ../../config/home/secrets.nix
+    ../../config/home/vscode.nix
+    ../../config/home/zsh.nix
+  ];
+}
