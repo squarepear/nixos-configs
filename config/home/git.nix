81638bfabec26d796896a976c62405617fa636b8
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2022-01-15 11:42:05 -0500

    add git lfs support

--- config/home/git.nix
+++ config/home/git.nix
@@ -7,6 +7,8 @@
     userName = "Jeffrey Harmon";
     userEmail = "16364318+SquarePear@users.noreply.github.com";
 
+    lfs.enable = true;
+
     signing = {
       signByDefault = true;
       key = "1A5CF61AC92D97AA";
