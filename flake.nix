eb8a3238ebe6b9af5bbf6511207198f135a8f63e
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2022-01-15 12:39:32 -0500

    switch to flake based config

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "NixOS configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, home-manager, nixpkgs }: {
+    nixosConfigurations = {
+      reshiram = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/reshiram
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.jeffrey = import ./hosts/reshiram/home.nix;
+          }
+        ];
+      };
+
+      shuckle = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/shuckle
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.jeffrey = import ./hosts/shuckle/home.nix;
+          }
+        ];
+      };
+
+      tepig = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./hosts/tepig
+
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.jeffrey = import ./hosts/tepig/home.nix;
+          }
+        ];
+      };
+    };
+  };
+}
