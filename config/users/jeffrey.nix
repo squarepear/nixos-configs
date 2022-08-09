b11d79ca4bb59f5b4336b45abdb009071c015689
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2022-08-09 13:25:24 -0400

    update ssh to only allow key based auth

--- config/users/jeffrey.nix
+++ config/users/jeffrey.nix
@@ -9,6 +9,11 @@
     home = "/home/jeffrey";
     shell = pkgs.zsh;
     uid = 1000;
+
+    # Authorized keys
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC6R0tD850LaMbSjrriau10t+onORK6r/SeGwawHEjUO 16364318+SquarePear@users.noreply.github.com"
+    ];
   };
 
   nix.settings.trusted-users = [
