6540bebdf22fdb90bd3d8d372c90146e21181bb9
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2022-08-12 20:07:26 -0400

    clean up tepig

--- hosts/tepig/default.nix
+++ hosts/tepig/default.nix
@@ -11,13 +11,10 @@
     ../../config/system
     ../../config/system/k3s/server.nix
     ../../config/system/backup.nix
-    ../../config/system/docker.nix
-    ../../config/system/glusterfs.nix
     ../../config/system/networking.nix
     ../../config/system/nfs.nix
     ../../config/system/ssh.nix
     ../../config/system/tailscale.nix
-    ../../config/system/usb.nix
 
     ../../config/users/jeffrey.nix
   ];
@@ -27,40 +24,6 @@
 
   # Don't use GUI
   system.gui.enable = false;
-
-  # Enable Minecraft Server
-  environment.systemPackages = with pkgs; [
-    jdk17
-  ];
-
-  systemd.services.minecraft-server = {
-    description = "Run the HFGS Minecraft server";
-
-    wants = [ "network.target" ];
-    after = [ "network.target" ];
-
-    unitConfig = {
-      Type = "simple";
-      RequiresMountsFor = "/cluster-nfs";
-    };
-
-    serviceConfig = {
-      WorkingDirectory = "/cluster-nfs/minecraft/survival-hfgs/";
-      ExecStart = "${pkgs.jdk17}/bin/java -Xmx4G --add-modules jdk.incubator.vector -jar fabric-server-mc.1.18.1-loader.0.12.12-launcher.0.10.2.jar nogui";
-    };
-
-    wantedBy = [ "multi-user.target" ];
-  };
-
-  # Enable NFS Server
-  services.nfs.server = {
-    enable = true;
-
-    exports = ''
-      /cluster-nfs 100.0.0.0/8(rw,sync,no_root_squash,insecure)
-    '';
-  };
-  networking.firewall.allowedTCPPorts = [ 2049 ];
 
   # Networking
   networking.firewall.trustedInterfaces = [ "eth0" "wlan0" ];
