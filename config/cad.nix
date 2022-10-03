032af46974b381940e3441edab2b125b567ee5b4
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2022-10-03 13:45:10 -0400

    updated flake

--- config/cad.nix
+++ config/cad.nix
@@ -2,7 +2,7 @@
 
 {
   my.home.packages = with pkgs; lib.mkIf config.system.gui.enable [
-    kicad-unstable # Electronics
+    # kicad-unstable # Electronics
     # freecad # 3D Design
     # prusa-slicer # 3D Printer Slicer
   ];
