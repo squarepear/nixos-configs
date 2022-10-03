562c4581044db59ffa85bb32d927a0230ce31143
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2022-10-03 13:45:48 -0400

    switch to new openrgb config

--- config/rgb.nix
+++ config/rgb.nix
@@ -1,18 +1,9 @@
 { pkgs, ... }:
 
 {
-  environment.systemPackages = [ pkgs.openrgb ];
-  services.udev.packages = [ pkgs.openrgb ];
+  services.hardware.openrgb = {
+    enable = true;
 
-  boot.kernelModules = [ "i2c-dev" "i2c-piix" ];
-
-  systemd.services.openrgb = {
-    description = "OpenRGB server daemon";
-    wantedBy = [ "multi-user.target" ];
-
-    serviceConfig = {
-      ExecStart = "${pkgs.openrgb}/bin/openrgb --server --server-port 6742";
-      Restart = "always";
-    };
+    motherboard = "amd";
   };
 }
