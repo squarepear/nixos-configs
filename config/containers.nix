af5ad250823ad569ca6ea923342c0b99894d02a0
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2023-05-18 14:11:19 -0400

    Update flakes

--- config/containers.nix
+++ config/containers.nix
@@ -6,16 +6,21 @@
     enable = true;
 
     dockerCompat = true;
+    defaultNetwork.settings.dns_enabled = true;
   };
 
   # Add extra packages
   environment.systemPackages = with pkgs; [
     distrobox
     podman-compose
+    fuse-overlayfs
   ];
 
   # Add podman group
   users.users."${config.user.name}" = {
     extraGroups = [ "podman" ];
   };
+
+  # Enable cgroups
+  boot.kernelParams = [ "cgroup_memory=1" "cgroup_enable=memory" ];
 }
