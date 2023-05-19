d29f13293b42fb6bcdf07f013290f7638426b253
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2023-05-19 11:34:47 -0400

    Fix tepig boot

--- hosts/tepig/hardware-configuration.nix
+++ hosts/tepig/hardware-configuration.nix
@@ -18,7 +18,7 @@
     {
       device = "/dev/disk/by-uuid/44444444-4444-4444-8888-888888888888";
       fsType = "ext4";
-      options = [ "autodefrag" "noatime" ];
+      options = [ "noatime" ];
     };
 
   swapDevices = [ ];
