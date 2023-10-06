691d32c01ae8985bfa29be8626e29700675ab161
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2023-10-05 22:52:13 -0400

    Update config

--- config/rgb.nix
+++ config/rgb.nix
@@ -3,6 +3,7 @@
 {
   services.hardware.openrgb = {
     enable = true;
+    package = pkgs.openrgb-with-all-plugins;
 
     motherboard = "amd";
   };
@@ -12,8 +13,48 @@
     enable = true;
 
     systemCronJobs = [
-      "0 0 * * *      ${config.user.name}    ${pkgs.openrgb}/bin/openrgb -p off"
+      "0 21 * * *      ${config.user.name}    ${pkgs.openrgb}/bin/openrgb -p off"
       "0 8 * * *      ${config.user.name}    ${pkgs.openrgb}/bin/openrgb -p on"
     ];
   };
+
+  # my.systemd.user.services.openrgb-on = {
+  #   Unit = { Description = "Turn on rgb lights"; };
+
+  #   Service = {
+  #     ExecStart = "${pkgs.openrgb}/bin/openrgb -p on";
+  #   };
+  # };
+
+  # my.systemd.user.timers.openrgb-on = {
+  #   Unit.Description = "Turn on rgb lights";
+
+  #   Timer = {
+  #     Unit = "openrgb-on.service";
+  #     OnCalendar = "08:00:00";
+  #     Persistent = true;
+  #   };
+
+  #   Install.WantedBy = [ "timers.target" ];
+  # };
+
+  # my.systemd.user.services.openrgb-off = {
+  #   Unit = { Description = "Turn off rgb lights"; };
+
+  #   Service = {
+  #     ExecStart = "${pkgs.openrgb}/bin/openrgb -p off";
+  #   };
+  # };
+
+  # my.systemd.user.timers.openrgb-off = {
+  #   Unit.Description = "Turn off rgb lights";
+
+  #   Timer = {
+  #     Unit = "openrgb-off.service";
+  #     OnCalendar = "21:00:00";
+  #     Persistent = true;
+  #   };
+
+  #   Install.WantedBy = [ "timers.target" ];
+  # };
 }
