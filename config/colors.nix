7094814e79e005578bc4fa2702332005b9a8b48a
Jeffrey Harmon <contact@jeffreyharmon.dev>
2024-06-04 21:55:40 -0400

    Update color scheme to evenok-dark

--- config/colors.nix
+++ config/colors.nix
@@ -6,8 +6,13 @@
       inputs.nix-colors.homeManagerModule
     ];
 
-    colorScheme = inputs.nix-colors.colorSchemes.equilibrium-dark;
-    # colorScheme = inputs.nix-colors.colorSchemes.tender;
-
+    # colorScheme = inputs.nix-colors.colorSchemes.ayu-dark;
+    # colorScheme = inputs.nix-colors.colorSchemes.catppuccin-mocha;
+    # colorScheme = inputs.nix-colors.colorSchemes.circus; # +
+    # colorScheme = inputs.nix-colors.colorSchemes.classic-dark;
+    colorScheme = inputs.nix-colors.colorSchemes.evenok-dark; # +, OLED black
+    # colorScheme = inputs.nix-colors.colorSchemes.gruvbox-material-dark-hard;
+    # colorScheme = inputs.nix-colors.colorSchemes.kanagawa; # +
+    # colorScheme = inputs.nix-colors.colorSchemes.material-darker; # +
   };
 }
