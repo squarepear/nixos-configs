4e9c2f1df35a7e9a8bc7f7d84e5e4a956944d16b
Jeffrey Harmon <contact@jeffreyharmon.dev>
2024-07-02 12:00:43 -0400

    Add initial agenix config

---
+++ secrets.nix
@@ -0,0 +1,18 @@
+let
+  jeffrey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPyZWg5m3pXHOqNfdrO6ecghFfQowb/Y7Df7otocETHq";
+
+  users = [ jeffrey ];
+
+  altaria = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJwtSK+fzHcdehRsrYSz/fqfiKvSQ9P6NZTUVwRKR9za";
+  reshiram = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE0BJ8FAFBISvFH60tAE+gYk84Qq3Q1lV6uxek0E+DLw";
+  tepig = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHNS2qVtpFUJqZYhrqtbaIXa9TgCvYQiQtf47tXM1iP0";
+  uxie = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIB7dRYCqWEzF4OUABaid16HXrzVvUx8YMii/FdSdOXvj";
+
+  hosts = [ altaria reshiram tepig uxie ];
+in
+{
+  "secrets/test-secret.age".publicKeys = users ++ hosts;
+
+  # User Specific
+  "secrets/jeffrey/passwordfile.age".publicKeys = [ jeffrey ] ++ hosts;
+}
