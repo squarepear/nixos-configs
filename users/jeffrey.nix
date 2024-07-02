34c831144fc44be56931e46cae50a5ebd366eac8
Jeffrey Harmon <contact@jeffreyharmon.dev>
2024-07-02 12:00:43 -0400

    Add initial agenix config

--- users/jeffrey.nix
+++ users/jeffrey.nix
@@ -6,6 +6,8 @@
     publickey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPyZWg5m3pXHOqNfdrO6ecghFfQowb/Y7Df7otocETHq";
   };
 
+  age.secrets.jeffrey-passwordfile.file = ../secrets/jeffrey/passwordfile.age;
+
   users.users."${config.pear.user.name}" = {
     isNormalUser = true;
     description = "Jeffrey Harmon";
@@ -14,6 +16,8 @@
     home = "/home/${config.pear.user.name}";
     shell = pkgs.zsh;
     uid = 1000;
+
+    hashedPasswordFile = config.age.secrets.jeffrey-passwordfile.path;
 
     # Authorized keys
     openssh.authorizedKeys.keys = [
