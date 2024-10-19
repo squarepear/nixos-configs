9ff29575b181a0e1f75f857e02452de7b710f982
Jeffrey Harmon <contact@jeffreyharmon.dev>
2024-10-19 18:31:29 -0400

    Update dock apps on host kyurem

--- darwin/system.nix
+++ darwin/system.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   security.pam.enableSudoTouchIdAuth = true;
@@ -8,6 +8,19 @@
     dock = {
       autohide = true;
       show-recents = false;
+      persistent-apps = [
+        "/Applications/Safari.app"
+        "/System/Applications/Launchpad.app"
+        "/System/Applications/Messages.app"
+        "/System/Applications/Mail.app"
+        "${pkgs.discord-canary}/Applications/Discord Canary.app"
+        "${pkgs.slack}/Applications/Slack.app"
+        "/System/Applications/Calendar.app"
+        "/System/Applications/Music.app"
+        "/System/Applications/System Settings.app"
+        "${pkgs.iterm2}/Applications/iTerm2.app"
+        "${pkgs.vscode}/Applications/Visual Studio Code.app"
+      ];
     };
     finder.CreateDesktop = false;
     loginwindow.GuestEnabled = false;
