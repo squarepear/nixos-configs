6c2b4f23b5d425d5d75fce763f0038e9d42743dd
Jeffrey Harmon <contact@jeffreyharmon.dev>
2024-10-19 18:31:29 -0400

    Update dock apps on host kyurem

--- darwin/pkgs.nix
+++ darwin/pkgs.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
   environment.systemPackages = with pkgs; [
@@ -40,25 +40,4 @@
     vscode
     zoom-us
   ];
-
-  # system.activationScripts.applications.text =
-  #   let
-  #     env = pkgs.buildEnv {
-  #       name = "system-applications";
-  #       paths = config.environment.systemPackages;
-  #       pathsToLink = "/Applications";
-  #     };
-  #   in
-  #   pkgs.lib.mkForce ''
-  #     # Set up applications.
-  #     echo "setting up /Applications..." >&2
-  #     rm -rf /Applications/Nix\ Apps
-  #     mkdir -p /Applications/Nix\ Apps
-  #     find ${env}/Applications -maxdepth 1 -type l -exec readlink '{}' + |
-  #     while read src; do
-  #       app_name=$(basename "$src")
-  #       echo "copying $src" >&2
-  #       ${pkgs.mkalias}/bin/mkalias "$src" "/Applications/Nix Apps/$app_name"
-  #     done
-  #   '';
 }
