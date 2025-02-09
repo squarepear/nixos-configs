c276bf1f11bbeec0b87a19d7f3c5c1a46a166d1d
Jeffrey Harmon <contact@jeffreyharmon.dev>
2025-02-09 13:31:43 -0500

    Update Hyprland configuration to use nwg-drawer

--- system/hyprland/default.nix
+++ system/hyprland/default.nix
@@ -36,13 +36,29 @@
 
     programs.hyprland = {
       enable = true;
+
       package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
       portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
     };
 
     my.wayland.windowManager.hyprland = {
       enable = true;
-      package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+
+      package = null;
+      portalPackage = null;
+    };
+
+    xdg.portal = {
+      enable = true;
+      xdgOpenUsePortal = true;
+      config = {
+        common.default = [ "gtk" ];
+        hyprland.default = [ "gtk" "hyprland" ];
+      };
+
+      extraPortals = [
+        pkgs.xdg-desktop-portal-gtk
+      ];
     };
   };
 }
