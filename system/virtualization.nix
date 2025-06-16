27123b0dd6c068414d795373100611014bf77a71
Jeffrey Harmon <contact@jeffreyharmon.dev>
2025-06-16 12:02:27 -0400

    Enhance virtualization configuration by enabling virt-manager and refining QEMU settings

--- system/virtualization.nix
+++ system/virtualization.nix
@@ -1,44 +1,28 @@
 { config, pkgs, ... }:
 
 {
-  # Enable libvirtd
+  # Enable libvirtd and virt-manager
   virtualisation.libvirtd = {
     enable = true;
-    extraConfig = ''
-      unix_sock_group = "libvirtd"
-      unix_sock_rw_perms = "0770"
-      log_filters="1:qemu"
-      log_outputs="1:file:/var/log/libvirt/libvirtd.log"
-    '';
-
-    onBoot = "ignore";
-    onShutdown = "shutdown";
-
     qemu = {
+      vhostUserPackages = with pkgs; [ virtiofsd ];
+      # Enable OpenGL and GPU acceleration
+      package = pkgs.qemu_kvm;
+      runAsRoot = false;
+      swtpm.enable = true;
       ovmf = {
         enable = true;
         packages = [ pkgs.OVMFFull.fd ];
       };
-      swtpm.enable = true;
-
-      verbatimConfig = ''
-        user = "${config.pear.user.name}"
-        group = "users"
-      '';
     };
   };
-
-  environment.systemPackages = with pkgs; [
-    virt-manager
-    libguestfs
-  ];
-
-  programs.dconf.enable = true; # Needed for saving settings in virt-manager
+  programs.virt-manager.enable = true;
 
   # Add libvirtd and tss group
   users.users."${config.pear.user.name}".extraGroups = [ "libvirtd" "kvm" "tss" ];
 
   # Boot configuration
   boot.kernelParams = [ "amd_iommu=on" "iommu=pt" "video=efifb:off" ];
-  boot.kernelModules = [ "kvm-amd" ];
+  boot.kernelModules = [ "kvm-amd" "vfio" "vfio_iommu_type1" "vfio_pci" "vfio_virqfd" ];
+
 }
