dce53363935fd2f19cf66a6643909464f628534f
Jeffrey Harmon <contact@jeffreyharmon.dev>
2025-06-26 22:31:24 -0400

    Enable open-webui service and configure environment variables

--- hosts/uxie/open-webui.nix
+++ hosts/uxie/open-webui.nix
@@ -2,8 +2,17 @@
 
 {
   services.open-webui = {
-    enable = false;
+    enable = true;
 
     port = 14141;
+    host = "0.0.0.0";
+    environment = {
+      ANONYMIZED_TELEMETRY = "False";
+      DO_NOT_TRACK = "True";
+      SCARF_NO_ANALYTICS = "True";
+      OLLAMA_API_BASE_URL = "http://reshiram:11434/API";
+      OLLAMA_BASE_URL = "http://reshiram:11434";
+      WEBUI_AUTH = "False";
+    };
   };
 }
