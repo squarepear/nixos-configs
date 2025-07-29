ee90b19669f783607453737ac53644155088fa56
Jeffrey Harmon <contact@jeffreyharmon.dev>
2025-07-29 13:34:47 -0400

    Update monitor configuration in Hyprland

--- system/hyprland/conf.nix
+++ system/hyprland/conf.nix
@@ -21,7 +21,7 @@ in
         # Monitor configuration
         monitor = [
           "DP-2,3840x2160@240,3840x0,1#, bitdepth,10, cm,wide"
-          "DP-1,3840x2160@60,0x0,1#, bitdepth,10"
+          "DP-3,3840x2160@60,0x0,1#, bitdepth,10"
           ",highrr,auto,1"
         ];
 
