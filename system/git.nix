6c737fde83ce3797d29df00415c5aedf0d7640c1
Jeffrey Harmon <contact@jeffreyharmon.dev>
2025-10-15 17:38:31 -0400

    Switch to gh for github auth

--- system/git.nix
+++ system/git.nix
@@ -1,23 +1,31 @@
 { ... }:
 
 {
-  my.programs.git = {
-    enable = true;
+  my.programs = {
+    git = {
+      enable = true;
 
-    userName = "Jeffrey Harmon";
-    userEmail = "contact@jeffreyharmon.dev";
+      userName = "Jeffrey Harmon";
+      userEmail = "contact@jeffreyharmon.dev";
 
-    lfs.enable = true;
+      lfs.enable = true;
 
-    signing = {
-      signByDefault = true;
-      key = "EC6381EC5C7904E8";
+      signing = {
+        signByDefault = true;
+        key = "EC6381EC5C7904E8";
+      };
+
+      extraConfig = {
+        init.defaultBranch = "main";
+        http.postBuffer = "524288000";
+      };
     };
 
-    extraConfig = {
-      init.defaultBranch = "main";
-      credential.helper = "store";
-      http.postBuffer = "524288000";
+    gh = {
+      enable = true;
+      gitCredentialHelper = {
+        enable = true;
+      };
     };
   };
 }
