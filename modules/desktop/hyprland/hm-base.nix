e3d69932fa1280c91337361bf241481b23a02257
Jeffrey Harmon <contact@jeffreyharmon.dev>
2026-02-09 23:27:58 -0500

    Fix evaluation warning for `system` replacement

--- modules/desktop/hyprland/hm-base.nix
+++ modules/desktop/hyprland/hm-base.nix
@@ -21,7 +21,7 @@ in
         portalPackage = null;
 
         plugins = lib.optionals cfg.enableSplitMonitorWorkspaces [
-          inputs.split-monitor-workspaces.packages.${pkgs.system}.split-monitor-workspaces
+          inputs.split-monitor-workspaces.packages.${pkgs.stdenv.hostPlatform.system}.split-monitor-workspaces
         ];
       };
 
