77d663183a367ee410d5bce505f3cf6376166606
Jeffrey Harmon <contact@jeffreyharmon.dev>
2026-05-18 07:59:38 -0400

    feat(ai): add impermanence settings

--- modules/services/ai.nix
+++ modules/services/ai.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pearlib,
   unstable,
   ...
 }:
@@ -11,19 +12,42 @@ let
 in
 {
   options.pear.services.ai = {
-    enable = lib.mkEnableOption "AI services support (Ollama)";
+    enable = lib.mkEnableOption "AI services support";
   };
 
   config = lib.mkIf cfg.enable {
+    home-manager.users = pearlib.perUser (name: {
+      home.packages = with unstable; [
+        opencode
+      ];
+    });
+
     services.ollama = {
       enable = true;
-      package = unstable.ollama;
+      package =
+        if vendor.gpu == "amd" then
+          unstable.ollama-rocm
+        else if vendor.gpu == "nvidia" then
+          unstable.ollama-cuda
+        else
+          unstable.ollama;
 
       host = "0.0.0.0";
       port = 11434;
+      home = "/persist/var/lib/ollama";
+
+      user = "ollama";
+      group = "ollama";
     };
 
     nixpkgs.config.rocmSupport = lib.mkIf (vendor.gpu == "amd") true;
     nixpkgs.config.cudaSupport = lib.mkIf (vendor.gpu == "nvidia") true;
+
+    pear.system.impermanence.users = pearlib.perUser (name: {
+      persist.directories = [
+        ".ollama"
+        ".config/opencode"
+      ];
+    });
   };
 }
